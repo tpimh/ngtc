dfa1150ef288a117fcb029f288168b6a4d0a46b7
Dmitry Golovin <dima@golovin.in>
2016-12-20 01:05:31 +0200

    introduce a variable that would simplify porting to different architetures in the future

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 DIR="$(dirname "$(readlink -f "$0")")"
+ARCH="x86_64"
+TARGETS="X86"
 
 cd "$DIR"/src
 git clone https://github.com/llvm-mirror/llvm
@@ -27,17 +29,17 @@ cmake \
     -DCLANG_DEFAULT_CXX_STDLIB=libc++ \
     -DCLANG_DEFAULT_LINKER=lld \
     -DCLANG_DEFAULT_RTLIB=compiler-rt \
-    -DLLVM_DEFAULT_TARGET_TRIPLE=x86_64-pc-linux-musl \
-    -DDEFAULT_SYSROOT="$DIR"/x86_64-pc-linux-musl/musl \
-    -DGCC_INSTALL_PREFIX="$DIR"/x86_64-pc-linux-musl/gcc-stub \
-    -DCMAKE_INSTALL_PREFIX="$DIR"/x86_64-pc-linux-musl/llvm \
+    -DLLVM_DEFAULT_TARGET_TRIPLE="$ARCH"-pc-linux-musl \
+    -DDEFAULT_SYSROOT="$DIR"/"$ARCH"-pc-linux-musl/musl \
+    -DGCC_INSTALL_PREFIX="$DIR"/"$ARCH"-pc-linux-musl/gcc-stub \
+    -DCMAKE_INSTALL_PREFIX="$DIR"/"$ARCH"-pc-linux-musl/llvm \
     -DCMAKE_BUILD_TYPE=Release \
-    -DLLVM_TARGETS_TO_BUILD=X86 \
+    -DLLVM_TARGETS_TO_BUILD="$TARGETS" \
     -G Ninja ..
 ninja
 ninja install
 
-PATH="$DIR"/x86_64-pc-linux-musl/llvm/bin:"$PATH"
+PATH="$DIR"/"$ARCH"-pc-linux-musl/llvm/bin:"$PATH"
 
 cd ../..
 git clone git://git.musl-libc.org/musl
@@ -51,16 +53,16 @@ cd build
 
 ../configure \
     CC='clang'
-    LIBCC="$DIR"/x86_64-pc-linux-musl/llvm/lib/clang/4.0.0/lib/linux/libclang_rt.builtins-x86_64.a \
-    --prefix="$DIR"/x86_64-pc-linux-musl/musl \
-    --syslibdir="$DIR"/x86_64-pc-linux-musl/musl/lib
+    LIBCC="$DIR"/"$ARCH"-pc-linux-musl/llvm/lib/clang/4.0.0/lib/linux/libclang_rt.builtins-"$ARCH".a \
+    --prefix="$DIR"/"$ARCH"-pc-linux-musl/musl \
+    --syslibdir="$DIR"/"$ARCH"-pc-linux-musl/musl/lib
 make
 make install
 
 cd ../../startup
-make
-make install DESTDIR="$DIR"/x86_64-pc-linux-musl/musl/lib
+make ARCH="$ARCH"
+make install ARCH="$ARCH" DESTDIR="$DIR"/"$ARCH"-pc-linux-musl/musl/lib
 
 cd ../hello
 clang -o hello hello.c
-"$DIR"/x86_64-pc-linux-musl/musl/lib/ld-musl-x86_64.so.1 ./hello
+"$DIR"/"$ARCH"-pc-linux-musl/musl/lib/ld-musl-"$ARCH".so.1 ./hello
