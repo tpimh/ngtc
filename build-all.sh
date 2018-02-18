08390ffaed8ddb500f283fd2b8850ad4628b09b0
Dmitry Golovin <dima@golovin.in>
2018-02-18 18:06:06 +0200

    now use unpatched version of apk and patched version of PRoot that supports it

--- build-all.sh
+++ build-all.sh
@@ -2,13 +2,17 @@
 
 ROOT='alpine'
 REPO='http://dl-cdn.alpinelinux.org/alpine/edge/main'
+APKVER='2.9.1-r0'
 
 rm -rf $ROOT
 mkdir -p $ROOT
-cp -r apk.static sync.sh build.sh patch $ROOT
+wget $REPO/x86_64/apk-tools-static-$APKVER.apk
+tar xf apk-tools-static-$APKVER.apk -C root sbin/apk.static 2>/dev/null
+rm apk-tools-static-$APKVER.apk
+cp -r root/* $ROOT
 
-./proot -S $ROOT /apk.static -X $REPO -U --no-cache --allow-untrusted --initdb add alpine-base
+./proot -S $ROOT /sbin/apk.static -X $REPO -U --no-cache --allow-untrusted --initdb add alpine-base
 ./proot -S $ROOT /bin/sh -c "echo $REPO > /etc/apk/repositories"
-./proot -S alpine /apk.static --no-cache add -t .llvmdeps git patch make cmake ninja clang-static clang-dev linux-headers g++ python
-./proot -S alpine /usr/bin/env -i /bin/sh -l /sync.sh
-./proot -S alpine /usr/bin/env -i /bin/sh -l /build.sh
+./proot -S $ROOT /sbin/apk --no-cache add -t .llvmdeps git patch make cmake ninja clang-static clang-dev linux-headers g++ python
+./proot -S $ROOT /usr/bin/env -i /bin/sh -l /sync.sh
+./proot -S $ROOT /usr/bin/env -i /bin/sh -l /build.sh
